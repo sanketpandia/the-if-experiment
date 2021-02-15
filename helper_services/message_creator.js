0648f9e777531382a7d05136b8e21e445e6d8f59
Sanket <M1053847@mindtree.com>
2021-02-15 14:58:56 +0530

    Semantic changes

--- helper_services/message_creator.js
+++ helper_services/message_creator.js
@@ -29,6 +29,35 @@ exports.createCmStatsMessage = async function (record, configFields, callsignFie
     return cmStatsResponse;
 }
 
+exports.createPilotStatsMessage = async function (record, configFields, callsignField) {
+    let dataFields = []
+    configFields.forEach(element => {
+        let data = record[element["airtableColumnName"]];
+        let fieldValue = data;
+        let inline_status = false;
+        if ("inline" in element && element["inline"] === "true") inline_status = true;
+        if ("type" in element && element["type"] === "array") {
+            fieldValue = data.join(", ");
+        }
+        if ("type" in element && element["type"] === "time") {
+            let time_left_str = (Math.floor(data / 3600)).toString() + ":"
+            time_left_str += (Math.floor((data % 3600) / 60)).toString().length === 1 ? "0" + (Math.floor((data % 3600) / 60)).toString() : (Math.floor((data % 3600) / 60)).toString()
+            fieldValue = time_left_str;
+        }
+        dataFields.push({
+            name: element["name"],
+            value: fieldValue,
+            inline: inline_status
+        })
+    });
+
+    var cmStatsResponse = new Discord.MessageEmbed()
+        .setTitle(`Pilot stats of  ${record[callsignField]}`)
+        .addFields(dataFields)
+        .setAuthor("TheAircraftExperimentBot")
+    return cmStatsResponse;
+}
+
 exports.createLiveMessage = async function (responseObj) {
 
     let responseMessages = []
@@ -54,6 +83,7 @@ exports.createLiveMessage = async function (responseObj) {
 
     return responseMessages;
 }
+
 
 exports.createLiveMiniMessage = async function (responseObj) {
 
